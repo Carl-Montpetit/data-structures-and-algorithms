30766c50f99a26b0ae5c5f67ea2d4383ce57343f
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-01-03 03:37:19 -0500

    updated destructor + remove method

--- node.hpp
+++ node.hpp
@@ -14,7 +14,7 @@ public:
 
   Node(const T content) : _next_node(nullptr), _content(content) {}
 
-  ~Node() { delete _next_node; }
+  ~Node() = default;
 
   Node<T> *get_next_node() const { return _next_node; }
 
@@ -23,6 +23,28 @@ public:
   T &get_content() { return _content; }
 
   void set_content(const T content) { _content = content; }
+
+  void remove() {
+    Node<T> *temp = this;
+
+    if (temp->_next_node) {
+      Node<T> *next_node = temp->_next_node;
+
+      if (next_node->get_next_node()) {
+        temp->_next_node = next_node->get_next_node();
+      } else {
+        temp->_next_node = nullptr;
+      }
+
+      temp->_content = next_node->get_content();
+      delete next_node;
+    } else {
+      // Handle the case where the node being removed is the last node
+      if (temp->_next_node) {
+        temp->_next_node = nullptr;
+      }
+    }
+  }
 };
 
 #endif
